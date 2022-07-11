cb8e3ce0cbb311b8d4171ad52c5d8d2e58e65818
Veysel <veyselkartalms@gmail.com>
2022-07-12 02:50:31 +0300

    map added

--- travel-advisor/src/components/List/List.jsx
+++ travel-advisor/src/components/List/List.jsx
@@ -1,4 +1,10 @@
+import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, Select } from "@material-ui/core";
+
+import useStyles from "./styles"; 
+
 const List = () => {
+    const classes = useStyles();
+    
     return (
         <h1>List</h1>
     );
